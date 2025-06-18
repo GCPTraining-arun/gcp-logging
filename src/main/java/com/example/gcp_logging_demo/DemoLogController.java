55ceb71e7d06737e0e8a204aafe5b8f571a14ecb
sreenath <rdgowda79@gmail.com>
2025-06-18 23:05:14 +0530

    First commit

---
+++ src/main/java/com/example/gcp_logging_demo/DemoLogController.java
@@ -0,0 +1,20 @@
+package com.example.gcp_logging_demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoLogController {
+    private static final Logger logger = LoggerFactory.getLogger(DemoLogController.class);
+
+    @GetMapping
+    public String info() {
+
+        logger.info("INFO log from Spring Boot");
+        logger.warn("WARN log from Spring Boot");
+        logger.error("ERROR log from Spring Boot");
+        return "GCP logging demo";
+    }
+}
---
+++ src/main/java/com/example/gcp_logging_demo/GcpLoggingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gcp_logging_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpLoggingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpLoggingDemoApplication.class, args);
+	}
+
+}
